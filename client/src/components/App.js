ea835d39fb13840d56cfda2d4faf3ac94b501b98
Ian Price <ianpriceve@gmail.com>
2019-03-04 14:40:04 -0700

    added scss to webpack and some basic styles to app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-
+import './App.scss';
 
 class App extends Component {
   render(){
     return (
       <div>
-        <h1>My React App</h1>
+        <h1>My React Apple! haahha</h1>
       </div>
     );
   };
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -15,6 +15,14 @@ module.exports ={
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.s?css/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
       }
     ]
   },
