a36d867d0b6b76adc68c9111d2fb46394f1ac7cc
Ian Price <ianpriceve@gmail.com>
2019-03-04 16:17:46 -0700

    fixed a class bug with class props babel plugin

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,13 +5,19 @@ import movie from '../assets/movie.mp4';
 
 
 class App extends Component {
+  state = {screen: 'landing'};
   render(){
     return (
       <div>
-        <h1>My React Apple! haahha</h1>
-        <div className='img'></div>
-        <img src={Tidus} alt='Tidus' />
-        <video src={movie} autoplay controls></video>
+        { this.state.screen == 'landing' ? (
+          <div>
+          <h1>My React Apple! haahha</h1>
+          <div className='img'></div>
+          <img src={Tidus} alt='Tidus' />
+          <video src={movie} autoplay controls></video>
+          </div>
+        ):
+        (null)}
       </div>
     );
   };
