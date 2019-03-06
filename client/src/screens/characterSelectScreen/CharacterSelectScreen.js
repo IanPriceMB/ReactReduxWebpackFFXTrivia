c90caf35247bb5fe290e2f3e64c3a147a5e145fe
Ian Price <ianpriceve@gmail.com>
2019-03-06 16:14:24 -0700

    a bit more styling and an update to the character select logic

--- client/src/screens/characterSelectScreen/CharacterSelectScreen.js
+++ client/src/screens/characterSelectScreen/CharacterSelectScreen.js
@@ -13,9 +13,7 @@ class CharacterSelectScreen extends Component{
     super(props)
 
     this.state= {
-      chosen: [],
-      count: null,
-
+      chosen: []
     }
   }
 
@@ -38,28 +36,40 @@ class CharacterSelectScreen extends Component{
   updateChosen = character => {
     const clicked = document.getElementById(character);
     const value = clicked.getAttribute("data-chosen");
-    if(value === "false"){
-      this.setState({chosen: [...this.state.chosen, character]})
-      clicked.style.borderColor = 'aqua'
-      clicked.setAttribute('data-chosen', "true")
+    if(this.state.chosen.length == 3){
+      document.getElementById(this.state.chosen[0]).setAttribute('data-chosen', "true");
+      document.getElementById(this.state.chosen[0]).style.borderColor = 'rgb(0, 0, 202)';
+      const byebye = this.state.chosen.splice(0,1);
+      this.setState({chosen: [...byebye, character]});
+      clicked.style.borderColor = 'aqua';
+      clicked.setAttribute('data-chosen', "true");
+
+    }
+    else if(value === "false"){
+      this.setState({chosen: [...this.state.chosen, character]});
+      clicked.style.borderColor = 'aqua';
+      clicked.setAttribute('data-chosen', "true");
     } else if (value === "true") {
       const unchosen = this.state.chosen.filter((chosenChar) => character !== chosenChar);
       this.setState({chosen: unchosen})
-      clicked.style.borderColor = 'rgb(0, 0, 202)'
+      clicked.style.borderColor = 'rgb(0, 0, 202)';
       clicked.setAttribute('data-chosen', "false")
     } 
   }
 
+  // Sets the game screen and the chosen characters to Redux state
+  startGame = () => {
 
+  }
 
   render() {
     return (
       <Fragment>
         <OptionsMenu></OptionsMenu>
         <div className='characterSelectScreen'>
-          <h1 className='characterSelectScreen__title'>Chose Your Characters ({this.state.count})</h1>
+          <h1 className='characterSelectScreen__title'>Chose Up to 3 Characters ({this.state.chosen.length})</h1>
           <CharacterSelectMenu pickable={this.props.available} updateChosen={this.updateChosen}></CharacterSelectMenu>
-          <h3 onClick>It's the right thing to do!</h3>
+          <button><h3 onClick>Let's Go!</h3></button>
         </div>
       </Fragment>
     )
