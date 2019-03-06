0e449bb12e9cec7a88445bb377976e7603962a4f
Ian Price <ianpriceve@gmail.com>
2019-03-06 13:20:54 -0700

    new sass for splash screen. will revisit style when game is done

--- client/src/screens/splashScreen/SplashScreen.js
+++ client/src/screens/splashScreen/SplashScreen.js
@@ -5,10 +5,12 @@ const SplashScreen = (props) => {
 
   return (
     <Fragment>
-      <div className='container'>
-        <h1>Final Fantasy X Trivia</h1>
-        <h3>Created by Ian Price</h3>
-        <button onClick={props.changeScreen.bind(this,'MainMenuScreen')}><h2>Blitz Off!</h2></button>
+      <div className='splash'>
+        <h1 className='splash__title'>Final Fantasy X Trivia</h1>
+        <h3 className='splash__subtitle'>Created by Ian Price</h3>
+        <button className='splash__button' onClick={props.changeScreen.bind(this,'MainMenuScreen')}>
+          <h2 className='splash__button__text'>Blitz Off!</h2>
+        </button>
       </div>
     </Fragment>
   )
